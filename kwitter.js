eb3ebd70a265be821262df4d57d79b982c04b40b
Winning0825 <83045678+Winning0825@users.noreply.github.com>
2022-02-16 17:23:21 -0800

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,12 @@
+ function addUser() {
+        
+    User_Name = document.getElementById("User_Name").value
+        
+            localStorage.setItem("User_Name", User_Name);
+            
+            window.location = "kwitter_room.html";
+    }
+     
+
+
+
